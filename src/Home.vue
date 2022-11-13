4ab40d9298db25877a88792fe88e8fd77ae7990e
nrstirzaker <nrstirzaker.techblog@gmail.com>
2022-11-13 12:22:52 +0000

    restarted work

---
+++ src/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h3>Hello World</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'Home',
+  components:{}
+})
